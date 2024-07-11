3240ebe9362b673e4d6fd9237b3c4fde5799232b
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-07-11 22:32:18 +0530

    Dahle Property Website search section

---
+++ Major-Project-05/script.js
@@ -0,0 +1,10 @@
+// search section
+
+document.querySelectorAll('.dropbtn').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.querySelector('.arrow').innerHTML = '<i class="ri-arrow-up-s-line"></i>'; // Up arrow
+    });
+    button.addEventListener('mouseout', () => {
+        button.querySelector('.arrow').innerHTML = '<i class="ri-arrow-down-s-line"></i>'; // Down arrow
+    });
+});
